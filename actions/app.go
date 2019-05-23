1ed3f82e04437541d34da80058ca0ae376c59d18
Jake <JayHay28324@gmail.com>
2019-05-23 15:16:11 -0400

    Restart back to 0 to try again with echo
    Completely restarting with echo as the base for the app.

--- actions/app.go
+++ actions/app.go
@@ -1,8 +1,1 @@
 package actions
-
-import (
-	"github.com/gobuffalo/envy"
-)
-
-// ENV determines if the environment is dev, test, or prod
-var ENV = envy.Get("GO_ENV", "development")
--- models/user.go
+++ models/user.go
@@ -1,7 +1,1 @@
 package models
-
-// User is login information
-type User struct {
-	Username string `db:"username"`
-	Secret   []byte `db:"secret"`
-}
