5d73da3b3c685510d464b0eafafb9afa398e4900
Jake <jayhay28324@gmail.com>
2019-05-23 22:21:57 -0400

    Starting from the beginning with echo

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+// StartPage is a start
+func StartPage(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", StartPage)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Logger.Fatal(e.Start(":1212"))
+}
