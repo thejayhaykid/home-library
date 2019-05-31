fed90496077d8bcb0023652405946d4ff3a99b0a
Jake J. Hayes <jhayes@hmbnet.com>
2019-05-31 15:13:25 -0400

    Using html template instead of just rendering a string

--- actions/app.go
+++ actions/app.go
@@ -1,22 +1,40 @@
 package actions
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+// Template is for rendering
+type Template struct {
+	templates *template.Template
+}
+
+// Render render's the template
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 // StartPage is a start
 func StartPage(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.Render(http.StatusOK, "index", "World")
 }
 
 // Start sets up the new instance of echo
 func Start() *echo.Echo {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("./templates/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.GET("/", StartPage)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Static("/assets", "assets")
 	return e
 }
